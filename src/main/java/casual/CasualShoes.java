c972a4b3247947c6ced015736fca6c398c9ba8bb
maradC <maradiagachristopher5@gmail.com>
2024-11-20 18:53:58 -0500

    adding comments

--- src/main/java/casual/CasualShoes.java
+++ src/main/java/casual/CasualShoes.java
@@ -3,6 +3,11 @@ package casual;
 import org.example.Shoes;
 
 public class CasualShoes implements Shoes {
+    /**
+     * Displays the name of the casual shoes product.
+     * This method prints "Casual Shoes" to the console, indicating that this
+     * is a casual version of the shoes product.
+     */
     @Override
     public void display(){
         System.out.println("Casual Shoes");
--- src/main/java/casual/CasualTop.java
+++ src/main/java/casual/CasualTop.java
@@ -3,6 +3,11 @@ package casual;
 import org.example.Top;
 
 public class CasualTop implements Top {
+    /**
+     * Displays the name of the casual top product.
+     * This method prints "Casual Top" to the console, indicating that this
+     * is a casual version of the top product.
+     */
     @Override
     public void display() {
         System.out.println("Casual Top");
