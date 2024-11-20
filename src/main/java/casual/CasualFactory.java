b8af59a7fd4c6a65d66f01fd1f123dd2db48a1a6
maradC <maradiagachristopher5@gmail.com>
2024-11-20 18:53:06 -0500

    adding comments

--- src/main/java/casual/CasualFactory.java
+++ src/main/java/casual/CasualFactory.java
@@ -7,19 +7,39 @@ import org.example.Top;
 
 public class CasualFactory implements OutfitRequirements {
 
-        @Override
-        public Top createTop() {
-            return new CasualTop();
-        }
-
-        @Override
-        public Pants createPants() {
-            return new CasualPants();
-        }
-
-        @Override
-        public Shoes createShoes() {
-            return new CasualShoes();
-        }
+    /**
+     * Creates a casual top garment.
+     * This method is responsible for creating a specific top product that is part of the Casual family.
+     *
+     * @return a {@link Top} object representing a casual top.
+     */
+    @Override
+    public Top createTop() {
+        // Return a new instance of CasualTop
+        return new CasualTop();
     }
 
+    /**
+     * Creates casual pants garment.
+     * This method is responsible for creating a specific pants product that is part of the Casual family.
+     *
+     * @return a {@link Pants} object representing casual pants.
+     */
+    @Override
+    public Pants createPants() {
+        // Return a new instance of CasualPants
+        return new CasualPants();
+    }
+
+    /**
+     * Creates casual shoes garment.
+     * This method is responsible for creating a specific shoes product that is part of the Casual family.
+     *
+     * @return a {@link Shoes} object representing casual shoes.
+     */
+    @Override
+    public Shoes createShoes() {
+        // Return a new instance of CasualShoes
+        return new CasualShoes();
+    }
+}
--- src/main/java/casual/CasualPants.java
+++ src/main/java/casual/CasualPants.java
@@ -3,6 +3,11 @@ package casual;
 import org.example.Pants;
 
 public class CasualPants implements Pants {
+    /**
+     * Displays the name of the casual pants product.
+     * This method prints "Casual Pants" to the console, indicating that this
+     * is a casual version of the pants product.
+     */
     @Override
     public void display(){
         System.out.println("Casual Pants");
